e30fab9a45e4e32ff86498ab4b23b1b16c859a48
atmyworks <shagocart@gmail.com>
2026-01-08 21:05:11 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-const container = document.getElementById('root');
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+)
 
-if (container) {
-  const root = ReactDOM.createRoot(container);
-  root.render(
-    <HashRouter>
-      <App />
-    </HashRouter>
-  )
-} else {
-  console.error("Root element not found");
-}
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
